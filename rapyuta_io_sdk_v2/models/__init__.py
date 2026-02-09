a3e77df7adbb98b550b2d845920035eaedc3ae35
rAJ-2301 <raj.kumar@rapyuta-robotics.com>
2026-02-09 17:07:00 +0530

    fix(fileupload): remove redundant request models

--- rapyuta_io_sdk_v2/models/__init__.py
+++ rapyuta_io_sdk_v2/models/__init__.py
@@ -107,11 +107,9 @@ from rapyuta_io_sdk_v2.models.serviceaccount import (
 from rapyuta_io_sdk_v2.models.fileupload import (
     FileUpload as FileUpload,
     FileUploadList as FileUploadList,
-    FileUploadRequest as FileUploadRequest,
     FileUploadSpec as FileUploadSpec,
     FileUploadStatus as FileUploadStatus,
     SharedURL as SharedURL,
     SharedURLList as SharedURLList,
-    SharedURLRequest as SharedURLRequest,
     SharedURLSpec as SharedURLSpec,
 )
